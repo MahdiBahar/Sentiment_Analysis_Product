60f5eeb97cdbe22cf5f5a1d2101c3fa06b121835
MahdiBahar <mahdi.bahar.g@gmail.com>
2024-12-22 11:18:14 +0330

    create check_add_url method in RPC server

--- RPC_client.py
+++ RPC_client.py
@@ -19,6 +19,7 @@ def make_request(method, params):
     response = requests.post(url, headers=headers, json=request_payload)
     
     # Handling the response
+    print(f"----status : {response.status_code}")
     if response.status_code == 200:
         response_json = response.json()
         print("Full response:", response_json) 
@@ -30,14 +31,34 @@ def make_request(method, params):
         raise Exception(f"HTTP Error: {response.status_code} - {response.text}")
 
 
+#####Test 
+
+# crawl_app_nickname ='squid game'
+# crawl_url = 'https://cafebazaar.ir/app/com.defugames.survivegame'
+# crawl_app_nickname = 'keyboard'
+# crawl_url ='https://cafebazaar.ir/app/com.ziipin.softkeyboard.iran'
+
+# crawl_app_nickname ='squid game'
+# crawl_url = 'https://www.geeksforgeeks.org/postgresql-drop-table'
+crawl_app_nickname= 'refah'
+crawl_url= "https://cafebazaar.ir/app/com.refahbank.dpi.android"
+# crawl_url = 'https://cafebazaar.ir/app/com.sibche.aspardproject.app'
+
+# crawl_url = 'https://cafebazaar.ir/app/com.sibche.aspardproject.app'
+
 
 if __name__ == "__main__":
     try:
-        app_ids = [9,10,11,12,13,14,15]  
-        result_comment_crawl = make_request("crawl_comment", {"app_ids": app_ids})
-        print(f"Result of crawl_comment: {result_comment_crawl}")
-        result_sentiment = make_request("sentiment_analysis", {"app_ids": app_ids})
-        print(f"Result of crawl_comment: {result_sentiment}")
+        # app_ids = [28]  
+        # result_comment_crawl = make_request("crawl_comment", {"app_ids": app_ids})
+        # print(f"Result of crawl_comment: {result_comment_crawl}")
+        
+        # result_sentiment = make_request("sentiment_analysis", {"app_ids": app_ids})
+        # print(f"Result of sentiment_comment: {result_sentiment}")
+        
+        result_check_add_url = make_request("check_add_url",{"crawl_app_nickname": crawl_app_nickname, "crawl_url": crawl_url})
+        print(f"Result of check url to add or ignore is that {result_check_add_url}")
+
     except Exception as e:
         print(f"Error: {e}")
 
--- RPC_server.py
+++ RPC_server.py
@@ -2,6 +2,7 @@ from jsonrpc import JSONRPCResponseManager, dispatcher
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 from comment_scraper import fetch_app_urls_to_crawl, crawl_comments
+from app_scraper_check import give_information_app, check_and_create_app_id
 from analyze_sentiment import analyze_and_update_sentiment
 
 class RequestHandler(BaseHTTPRequestHandler):
@@ -16,30 +17,48 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(response.json.encode())
 
 
-
 @dispatcher.add_method
 def crawl_comment(app_ids):
     # app_ids = [28,31]
     apps = fetch_app_urls_to_crawl(app_ids)
-    for app_id, app_url in apps:
-        print(f"Crawling comments for app at {app_url}")
-        crawl_comments(app_id, app_url)
-    
-    return {"status": "success", "app_id": app_id}
+    try:
 
+        for app_id, app_url in apps:
+            print(f"Crawling comments for app at {app_url}")
+            crawl_comments(app_id, app_url)
+        return {"status": "success", "app_id": app_id}
+    
+    except Exception as e:
+
+        print(e)
+        return {"status": "failed"}
 
 @dispatcher.add_method
 def sentiment_analysis(app_ids):
     try:
 
         analyze_and_update_sentiment(app_ids)
+
+        return {"status of sentiment analysis": "success", "app_id":app_ids}
+    
     except KeyboardInterrupt:
         print("Script interrupted. Exiting gracefully....")
 
-    return {"status of sentiment analysis": "success"}
+        return {"status of sentiment analysis": "failed"}
 
+# By considering getting nickname
+@dispatcher.add_method
+def check_add_url(crawl_url, crawl_app_nickname = 'unknown'):
+    selected_url = crawl_url.split('/')[2]
 
-
+    if selected_url == "cafebazaar.ir":
+        app_data = give_information_app(crawl_app_nickname, crawl_url)
+        report = check_and_create_app_id(app_data)
+        print(report)
+    else:
+        report = f'The {crawl_url} is not related to Cafebazar or not valid. Please try again'
+        print(report)
+    return report
 
 # Run server
 if __name__ == "__main__":
